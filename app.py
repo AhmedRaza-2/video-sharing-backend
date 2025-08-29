a18864099fed0c620649317bb129ed245b19cf28
Ahmed Raza <araza2125012.pgc@gmail.com>
2025-08-29 17:01:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -126,9 +126,6 @@ def upload():
 @login_required
 def view_videos():
     return render_template('viewer.html', videos=videos)
-
-import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
